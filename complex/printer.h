f00bdd985dee074b7969046a72971c85904322d5
Arseniy <lazo07032005@gmail.com>
2022-01-18 15:35:01 +0300

    Добавьте файлы проекта.

---
+++ complex/printer.h
@@ -0,0 +1,18 @@
+/*
+#pragma once
+#include <fstream>
+
+class printer
+{
+public:
+	virtual void write(const expr& expression);
+
+protected:
+	std::ostream out;
+};
+
+class laTEX_printer : public printer
+{
+	virtual void write(const expr& expression);
+};
+*/
